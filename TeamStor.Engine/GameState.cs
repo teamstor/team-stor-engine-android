3250b56ce773e8ae46e428d2af301ebac1f04fa0
Hannes Mann <hannesmann2000@gmail.com>
2018-01-20 01:12:02 +0100

    Change GameState to abstract class

--- TeamStor.Engine/GameState.cs
+++ TeamStor.Engine/GameState.cs
@@ -11,7 +11,7 @@ namespace TeamStor.Engine
     /// <summary>
     /// State of the game (such as main menu, in-game, etc)
     /// </summary>
-    public class GameState
+    public abstract class GameState
     {
         /// <summary>
         /// Game class of this state.
@@ -30,19 +30,13 @@ namespace TeamStor.Engine
         /// Called when the state is entered.
         /// </summary>
         /// <param name="previousState">The previous state, can be null</param>
-        public virtual void OnEnter(GameState previousState)
-        {
-
-        }
+        public abstract void OnEnter(GameState previousState);
 
         /// <summary>
         /// Called when the state is left.
         /// </summary>
         /// <param name="previousState">The next state, can be null</param>
-        public virtual void OnLeave(GameState nextState)
-        {
-
-        }
+        public abstract void OnLeave(GameState nextState);
 
         /// <summary>
         /// Called once every frame.
@@ -50,28 +44,19 @@ namespace TeamStor.Engine
         /// <param name="deltaTime">The amount of time since the last frame, in seconds.</param>
         /// <param name="totalTime">The total amount of time (in seconds) since the game started.</param>
         /// <param name="count">The total amount of updates since the game started.</param>
-        public virtual void Update(double deltaTime, double totalTime, long count)
-        {
-
-        }
+        public abstract void Update(double deltaTime, double totalTime, long count);
 
         /// <summary>
         /// Called 60 (change with <code>Game.FixedUpdatesPerSecond</code>) times a second.
         /// </summary>
         /// <param name="count">The total amount of fixed updates since the game started.</param>
-        public virtual void FixedUpdate(long count)
-        {
-
-        }
+        public abstract void FixedUpdate(long count);
 
         /// <summary>
         /// Called once every frame to draw.
         /// </summary>
         /// <param name="batch">The sprite batch to draw with.</param>
         /// <param name="screenSize">The size of the screen.</param>
-        public virtual void Draw(SpriteBatch batch, Vector2 screenSize)
-        {
-
-        }
+        public abstract void Draw(SpriteBatch batch, Vector2 screenSize);
     }
 }
